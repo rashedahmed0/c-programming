dad67945571b3dac476476c84afcf2eb984754d2
Rialahmed0 <rashedahommod0@gmail.com>
2024-12-05 21:42:25 +0600

    case

--- hello.c
+++ hello.c
@@ -2,9 +2,14 @@
 
 int main()
 {
-    printf("hello world \n");
-    printf("hello world \n");
-    printf("hello world \n");
-    printf("hello world \n");
+    int age = 24;
+    printf("age is %d \n", age);
+
+    float pi = 3.1416;
+    printf("pi value is %f \n", pi);
+
+    char star = '*';
+    printf("star look like %c \n", star);
+
     return 0;
 };
