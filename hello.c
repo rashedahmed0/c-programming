9480022fd3180e55e978217c3b13983750117e58
Rialahmed0 <rashedahommod0@gmail.com>
2024-12-05 22:09:45 +0600

    first c problem

--- hello.c
+++ hello.c
@@ -4,10 +4,13 @@ int main()
 {
 
     // input
-    int age;
-    printf("add your age ; ");
-    scanf("%d", &age);
-    printf("your age is : %d", age);
 
+    int number1, number2;
+    printf("enter the first number :");
+    scanf("%d", &number1);
+    printf("enter the second number :");
+    scanf("%d", &number2);
+    int sum = number1 + number2;
+    printf("sum of to number is : %d", sum);
     return 0;
 };
