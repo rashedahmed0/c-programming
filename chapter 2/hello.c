a3f2a4a91115bab4835f469aacb8d62061649d5e
Rialahmed0 <rashedahommod0@gmail.com>
2024-12-10 00:40:05 +0600

    Qs.7 (a) write a program print the average of 3 number

--- chapter 2/hello.c
+++ chapter 2/hello.c
@@ -3,10 +3,12 @@
 
 int main()
 {
-    // Qs.6 (c)if a number is greater then 9 and less then 100
+    // Qs.7 (a) write a program print the average of 3 number
     int x = 10;
+    int y = 5;
+    int z = 15;
 
-    printf("%d", x > 9 && x < 100);
+    printf("%d", (x + y + z) / 3);
 
     return 0;
 }
