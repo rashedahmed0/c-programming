c4d33cf072d6aa8ce431ee0be1da49de430d388f
Rialahmed0 <rashedahommod0@gmail.com>
2024-12-10 00:18:21 +0600

    Qs.6 (c)if a number is greater then 9 and less then 100

--- chapter 2/hello.c
+++ chapter 2/hello.c
@@ -3,10 +3,10 @@
 
 int main()
 {
-    // Qs.6 (b)if it monday or its rainning
-    int isMonday = 0;
-    int isRaining = 1;
-    printf("%d", isMonday || isRaining);
+    // Qs.6 (c)if a number is greater then 9 and less then 100
+    int x = 10;
+
+    printf("%d", x > 9 && x < 100);
 
     return 0;
 }
