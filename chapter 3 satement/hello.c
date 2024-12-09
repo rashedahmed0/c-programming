e18da1294be8f734e6ef72b4f727cf0635aaa3b4
Rialahmed0 <rashedahommod0@gmail.com>
2024-12-10 01:13:56 +0600

    ternary operator

--- chapter 3 satement/hello.c
+++ chapter 3 satement/hello.c
@@ -5,18 +5,8 @@ int main()
     int age;
     printf("enter  your age : ");
     scanf("%d", &age);
-    if (age >= 18)
-    {
-        printf("you age adult \n");
-    }
-    else if (age > 13 && age < 18)
-    {
-        printf("you are teenager ");
-    }
-    else
-    {
-        printf("child");
-    }
+
+    age > 18 ? printf("adult") : printf("not adult");
 
     return 0;
 }
