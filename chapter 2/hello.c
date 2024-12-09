ea2886f0126b54e35309c454efab5c2e3e5484ad
Rialahmed0 <rashedahommod0@gmail.com>
2024-12-09 23:59:57 +0600

    Qs.5 Write a program if number divided by 2 or not

--- chapter 2/hello.c
+++ chapter 2/hello.c
@@ -3,19 +3,11 @@
 
 int main()
 {
-    //  assignment operator
-    int a = 2;
-    int b = 5;
-    a += b;
-    printf("%d \n", a);
-    a -= b;
-    printf("%d \n", a);
-    a *= b;
-    printf("%d \n", a);
-    a /= b;
-    printf("%d \n", a);
-    a %= b;
-    printf("%d \n", a);
+    // Qs.5 Write a program if number divided by 2 or not
+    int x;
+    printf("enter number : ");
+    scanf("%d", &x);
+    printf("%d \n", x % 2 == 0);
 
     return 0;
 }
