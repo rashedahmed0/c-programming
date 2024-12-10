5757868f084f7ee56030e22ffa9a642c0136abeb
Rialahmed0 <rashedahommod0@gmail.com>
2024-12-10 15:37:00 +0600

    print 1 to 10 without 6 .. continue and breack

--- chapter 4 loop/hello.c
+++ chapter 4 loop/hello.c
@@ -3,14 +3,21 @@
 int main()
 
 {
-    // print a table number of take user number
-    int n;
-    printf("enter your number : ");
-    scanf("%d", &n);
+    // print 1 to 10 without 6
     for (int i = 1; i <= 10; i++)
     {
-        int table = n * i;
-
-        printf("%d \n", table);
+        if (i == 6)
+        {
+            continue;
+        }
+        printf("%d \n", i);
+    }
+    for (int i = 1; i <= 10; i++)
+    {
+        if (i == 6)
+        {
+            break;
+        }
+        printf("%d \n", i);
     }
 }
