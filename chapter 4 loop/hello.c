6826e6a726e49e54e9bea599eb00e8854677d90c
Rialahmed0 <rashedahommod0@gmail.com>
2024-12-10 14:05:27 +0600

    print 1 to 100

--- chapter 4 loop/hello.c
+++ chapter 4 loop/hello.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main()
+
 {
-    for (int i = 0; i < 5; i++)
+    // print 1 to 100
+    for (int i = 1; i <= 100; i++)
     {
-        printf("hello world %d \n", i);
+        printf("%d \n", i);
     }
     return 0;
 }
