92d9ead7491d1aa644ba319eeacfb82e020a2ca9
Rialahmed0 <rashedahommod0@gmail.com>
2024-12-10 18:56:49 +0600

    function

---
+++ chapter 5 function/hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// function declaretion
+void printHello();
+int main()
+{
+
+    // function call
+    printHello();
+    return 0;
+}
+
+// function definetion
+void printHello()
+{
+    printf("hello every one");
+}
